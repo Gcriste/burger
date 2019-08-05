3a85d66dd8e9bd6a006a52e2e16ff92bc5271a88
Griffin <griffincriste@gmail.com>
2019-08-05 15:00:51 -0500

    basic functional to sequelize

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var PORT = process.env.PORT || 8080;
 
 var app = express();
-
+var db = require("./models");
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
@@ -17,12 +17,11 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controllers.js");
+require("./routes/api-routes.js")(app);
 
-app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
